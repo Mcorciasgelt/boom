04a98a8ca186d3204d339d7ab3592fb12a55341b
Michael Corcias <michael.corcias@gelt.com>
2024-11-25 15:35:29 +0100

    entregable

--- js/script.js
+++ js/script.js
@@ -2,27 +2,37 @@
 const userInpunt = document.getElementById("userInput")
 const countdown = document.getElementById("countdown")
 const result = document.getElementById("result")
-/* const resart = getElementById("restart") */
+const restart = document.getElementById("restart")
 
 
-const jugadaAleatoria = new Promise((resolve) => {
-    setTimeout(() => {
-        const numeroAleatorio = Math.floor(Math.random()*3)+1
-        resolve(numeroAleatorio)
-    }, 5000)
-})
+intervalJuego = setInterval(evaluarJuego, 6000)
 
-setInterval(evauarJuego, 5000)
-
-function evauarJuego() {
+function evaluarJuego() {
+    result.classList.remove("explotado", "salvado")
     const userNumero = parseInt(userInpunt.value)
-    jugadaAleatoria.then((jugada)  => {
-    if(userNumero===jugada){
-        result.innerText = `Has salvado al mundo porque ${jugada} es igual a ${userNumero}`
-    }
-    else{
-        result.innerText = `La bomba ha explotado porque ${jugada} es diferente a ${userNumero}`
-    }
-})
+    const jugadaAleatoria = Math.floor(Math.random()*3)+1
 
+    if(!userNumero || userNumero < 1 || userNumero > 3) {
+        result.innerText = "Debes introducir un número válido entre 1 y 3"
+    }
+    
+    else if(userNumero===jugadaAleatoria){
+        result.innerText = `Has salvado al mundo porque ${jugadaAleatoria} es igual a ${userNumero}`
+        result.classList.add("salvado")
+    }
+
+    else{
+        result.innerText = `La bomba ha explotado porque ${jugadaAleatoria} es diferente a ${userNumero}`
+        result.classList.add("explotado")
+    }
 }
+
+
+function restartJuego() {
+    clearInterval(intervalJuego)
+    userInpunt.value = ""
+    result.innerText = ""
+    intervalJuego = setInterval(evaluarJuego, 6000)
+}
+
+restart.addEventListener("click",restartJuego)
